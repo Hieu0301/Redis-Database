8f691eef43d449dd1a4a3ef49b262fc9d39c576f
Hieu0301 <phamminhhieu.03012002@gmail.com>
2024-03-17 19:38:28 +0700

    first commit

---
+++ src/main/java/com/fit/se/repository/EmployeeRepository.java
@@ -0,0 +1,50 @@
+package com.fit.se.repository;
+
+import com.fit.se.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeRepository {
+
+    private HashOperations hashOperations;
+    private RedisTemplate redisTemplate;
+    @Autowired
+    private EmployeeMongoRepository employeeMongoRepository;
+
+    @Autowired
+    public EmployeeRepository(RedisTemplate redisTemplate) {
+        this.hashOperations = redisTemplate.opsForHash();
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void saveEmployee(Employee employee) {
+        hashOperations.put("EMPLOYEE", employee.getId(), employee);
+        employeeMongoRepository.save(employee);
+    }
+
+    public List<Employee> findAll() {
+        return hashOperations.values("EMPLOYEE");
+    }
+
+    @Cacheable(value = "employees", key = "#id")
+    public Employee findById(Integer id) {
+        return (Employee) hashOperations.get("EMPLOYEE", id);
+    }
+
+    public void update(Employee employee) {
+        saveEmployee(employee);
+        employeeMongoRepository.save(employee);
+    }
+
+    public void delete(Integer id) {
+        hashOperations.delete("EMPLOYEE", id);
+        employeeMongoRepository.deleteById(id);
+    }
+
+}
